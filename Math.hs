00c3a39a06ef7b412413f076da3b2598c195106e
Roald Frederickx <roald.frederickx@gmail.com>
2011-11-07 23:56:37 +0100

    initial math file

---
+++ Math.hs
@@ -0,0 +1,47 @@
+module Math where
+
+-- floating point format, easily switch between Float and Double
+type Flt = Double 
+
+type Point2D = (Flt, Flt)
+type Point3D = (Flt, Flt, Flt)
+type Vector = (Flt, Flt, Flt)
+
+data Object = Sphere Flt Point3D
+	| Triangle Point3D Point3D Point3D
+
+data Ray = Ray Point3D Vector
+
+
+
+-- TODO: penalty of having these simple functions in a separate module (= 
+-- no inlining (?)). Try to merge them later, or do a whole program 
+-- complisation somehow
+
+-- vim: expandtabs
+
+-- works for Point3D and Vector
+(.+.) :: (Flt, Flt, Flt) -> (Flt, Flt, Flt)
+	-> (Flt, Flt, Flt)
+(x1, y1, z1) .+. (x2, y2, z2) = (x1 + x2, y1 + y2, z1 + z2) 
+
+
+(.-.) :: (Flt, Flt, Flt) -> (Flt, Flt, Flt)
+	-> (Flt, Flt, Flt)
+(x1, y1, z1) .-. (x2, y2, z2) = (x1 - x2, y1 - y2, z1 - z2) 
+
+(.*.) :: (Flt, Flt, Flt) -> (Flt, Flt, Flt)
+	-> Flt
+(x1, y1, z1) .*. (x2, y2, z2) = x1*x2 + y1*y2 + z1*z2 
+
+(.*) :: (Flt, Flt, Flt) -> Flt
+	-> (Flt, Flt, Flt)
+(x, y, z) .* a = (a*x, a*y, a*z)
+
+(*.) :: Flt -> (Flt, Flt, Flt)
+	-> (Flt, Flt, Flt)
+a *. (x, y, z) = (a*x, a*y, a*z)
+
+len :: Vector -> Flt
+len v = sqrt(v .*. v)
+
