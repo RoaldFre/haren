391bfc94659919a9dfc37a4e8bbebb0064d9ffa2
Roald Frederickx <roald.frederickx@gmail.com>
2011-11-08 10:34:53 +0100

    tabs expanded

--- Math.hs
+++ Math.hs
@@ -8,7 +8,7 @@ type Point3D = (Flt, Flt, Flt)
 type Vector = (Flt, Flt, Flt)
 
 data Object = Sphere Flt Point3D
-	| Triangle Point3D Point3D Point3D
+        | Triangle Point3D Point3D Point3D
 
 data Ray = Ray Point3D Vector
 
@@ -18,30 +18,30 @@ data Ray = Ray Point3D Vector
 -- no inlining (?)). Try to merge them later, or do a whole program 
 -- complisation somehow
 
--- vim: expandtabs
 
 -- works for Point3D and Vector
 (.+.) :: (Flt, Flt, Flt) -> (Flt, Flt, Flt)
-	-> (Flt, Flt, Flt)
+        -> (Flt, Flt, Flt)
 (x1, y1, z1) .+. (x2, y2, z2) = (x1 + x2, y1 + y2, z1 + z2) 
 
 
 (.-.) :: (Flt, Flt, Flt) -> (Flt, Flt, Flt)
-	-> (Flt, Flt, Flt)
+        -> (Flt, Flt, Flt)
 (x1, y1, z1) .-. (x2, y2, z2) = (x1 - x2, y1 - y2, z1 - z2) 
 
 (.*.) :: (Flt, Flt, Flt) -> (Flt, Flt, Flt)
-	-> Flt
+        -> Flt
 (x1, y1, z1) .*. (x2, y2, z2) = x1*x2 + y1*y2 + z1*z2 
 
 (.*) :: (Flt, Flt, Flt) -> Flt
-	-> (Flt, Flt, Flt)
+        -> (Flt, Flt, Flt)
 (x, y, z) .* a = (a*x, a*y, a*z)
 
 (*.) :: Flt -> (Flt, Flt, Flt)
-	-> (Flt, Flt, Flt)
+        -> (Flt, Flt, Flt)
 a *. v = v .* a
 
 len :: Vector -> Flt
 len v = sqrt(v .*. v)
 
+-- vim: expandtab
