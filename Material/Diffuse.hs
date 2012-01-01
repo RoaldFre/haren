9087f7a3484d1e3e419797323345a49994d3feef
Roald Frederickx <roald.frederickx@gmail.com>
2012-01-01 02:59:56 +0100

    Diffuse material more robust for funky rays
    eg when it's combined with a dielectric material, or when camera in inside...

--- Material/Diffuse.hs
+++ Material/Diffuse.hs
@@ -10,7 +10,9 @@ mkDiffuse :: AnyMat
 mkDiffuse = MkAnyMat Diffuse
 
 instance Material Diffuse where
-    colorMaterialSingleLight int Diffuse (ilDir, ilCol) =
-        return $ ilCol .* (ilDir .*. (intNorm int))
+    colorMaterialSingleLight int Diffuse (ilDir, ilCol)
+        | cosTheta < 0 = return black -- Possible if we got hit from inside.
+        | otherwise    = return $ ilCol .* cosTheta
+        where cosTheta = ilDir .*. (intNorm int)
 
 -- vim: expandtab smarttab sw=4 ts=4
